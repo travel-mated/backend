f928ad73fe0e1226baf5be9dafd2fcaeb7a28a47
sunwon <jsw5913@naver.com>
2024-04-06 12:36:23 +0900

    feat(ResponseForm): ResponseForm 및 BaseEntity 정의

---
+++ src/main/java/com/tripmate/tripmate/common/CustomException.java
@@ -0,0 +1,23 @@
+package com.tripmate.tripmate.common;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class CustomException extends RuntimeException {
+    private final HttpStatus httpStatus; // HTTP 상태 코드
+    private final ResultCode resultCode; // 커스텀 결과 코드
+    private final String resultMessage; // 결과 메세지
+
+    public CustomException(HttpStatus httpStatus, ResultCode resultCode) {
+        this(httpStatus, resultCode, resultCode.getMessage());
+    }
+
+    public CustomException(HttpStatus httpStatus, ResultCode resultCode, String resultMessage) {
+        super(resultMessage);
+        this.httpStatus = httpStatus;
+        this.resultCode = resultCode;
+        this.resultMessage = resultMessage;
+    }
+
+}
